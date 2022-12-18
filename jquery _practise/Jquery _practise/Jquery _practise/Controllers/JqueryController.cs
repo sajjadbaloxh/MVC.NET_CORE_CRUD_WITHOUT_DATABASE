38590b7106d0c987767f010e5faa30334e8a422d
SAJJAD HUSSAIN <104603874+sajjadbaloxh@users.noreply.github.com>
2022-12-18 15:37:47 +0500

    jquery

---
+++ jquery _practise/Jquery _practise/Jquery _practise/Controllers/JqueryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Jquery__practise.Controllers
+{
+    public class JqueryController : Controller
+    {
+        public IActionResult jquery()
+        {
+            return View();
+        }
+    }
+}
---
+++ jquery _practise/Jquery _practise/Jquery _practise/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jquery__practise.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
